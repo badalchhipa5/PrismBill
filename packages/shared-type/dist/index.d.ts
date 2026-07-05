51473afc94b5a9e7dba4df7ab297ef9d59a7598f
badalchhipa5 <badalchhipa5@gmail.com>
2026-07-05 15:06:45 +0530

    chore: add workflow and build output

---
+++ packages/shared-type/dist/index.d.ts
@@ -0,0 +1,11 @@
+export interface BillSplit {
+    id: string;
+    amount: number;
+    currency: string;
+    participants: string[];
+}
+export interface SplitResult {
+    total: number;
+    perPerson: number;
+    currency: string;
+}
